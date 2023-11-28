c167c62be073004c7ae1e443e3780a77c853dd7c
kelvin kimani <kevohkyme35@gmail.com>
2023-11-28 22:02:14 +0300

    Add sidebar filter

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Sidebar.css";
-import Button from "../shared/components/formElements/Button";
+//import Button from "../shared/components/formElements/Button";
+import Electronics from "./electronics/Electronics";
 function Sidebar() {
   return (
     <div className="sidebar_container">
       <button className="department_btn">Departments</button>
+      <div className="categories">
+        <Electronics />
+      </div>
     </div>
   );
 }
